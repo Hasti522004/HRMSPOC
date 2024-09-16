01eda7df188c7783436b912fc6329881fdbe788d
Hasti522004 <hasti.hajipara@gmail.com>
2024-09-16 11:56:18 +0530

    API : Create Models

--- HRMSPOC.API/Models/Employee.cs
+++ HRMSPOC.API/Models/Employee.cs
@@ -1,7 +1,23 @@
-﻿namespace HRMSPOC.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRMSPOC.API.Models
 {
     public class Employee
     {
-
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+        public string Address { get; set; }
+        [Phone]
+        [Required]
+        public string PhoneNumber { get; set; }
+        public int OrganizationId { get; set; }
+        public Organization Organization { get; set; }
     }
 }
--- HRMSPOC.API/Models/Organization.cs
+++ HRMSPOC.API/Models/Organization.cs
@@ -1,6 +1,18 @@
-﻿namespace HRMSPOC.API.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRMSPOC.API.Models
 {
     public class Organization
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+
+        //Navigation Property
+        public ICollection<Employee> Employees { get; set; }
     }
 }
