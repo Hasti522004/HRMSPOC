727a99aa368f61ce04c02e7a4e4a22006e6021d5
Hasti522004 <hasti.hajipara@gmail.com>
2024-09-16 12:08:17 +0530

    API : Set up DbContext Class

--- HRMSPOC.API/Data/HRMSDbContext.cs
+++ HRMSPOC.API/Data/HRMSDbContext.cs
@@ -1,6 +1,25 @@
-﻿namespace HRMSPOC.API.Data
+﻿using HRMSPOC.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRMSPOC.API.Data
 {
-    public class HRMSDbContext
+    public class HRMSDbContext : DbContext
     {
+        public HRMSDbContext(DbContextOptions<HRMSDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Organization> Organization { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .HasOne(e => e.Organization)
+                .WithMany(o => o.Employees)
+                .HasForeignKey(e => e.Organization.Id);
+
+        }
     }
 }
